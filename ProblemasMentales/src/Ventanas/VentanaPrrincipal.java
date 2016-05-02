139bf678af32ebb393c13a253074925a47708d5f
Paco Gomez <fjgomez@florida-uni.es>
2016-05-02 16:02:20 +0200

    Eliminado el main de las ventanas

--- ProblemasMentales/src/Ventanas/VentanaPrrincipal.java
+++ ProblemasMentales/src/Ventanas/VentanaPrrincipal.java
@@ -12,22 +12,6 @@ public class VentanaPrrincipal extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaPrrincipal frame = new VentanaPrrincipal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
 	public VentanaPrrincipal() {
